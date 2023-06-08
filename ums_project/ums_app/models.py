284d239edd6e32f3a9c34e676b729902423972ac
Himanshu Bobade <himanshubobade96@gmail.com>
2023-06-08 16:40:10 +0530

    updated models and fields in models.py

--- ums_project/ums_app/models.py
+++ ums_project/ums_app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+
 # Create your models here.
+class Task(models.Model):
+    STATUS_CHOICES = (
+        ('Incomplete', 'Incomplete'),
+        ('In Progress', 'In Progress'),
+        ('Completed', 'Completed'),
+    )
+
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    due_date = models.DateField()
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES)
+
+    def __str__(self):
+        return self.title
