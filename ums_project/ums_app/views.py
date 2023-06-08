24207888f03b0d43b5b3ea95adb192e97aa6cc8b
Himanshu Bobade <himanshubobade96@gmail.com>
2023-06-08 20:38:57 +0530

    added task search functionality

--- ums_project/ums_app/views.py
+++ ums_project/ums_app/views.py
@@ -14,11 +14,15 @@ def create_task(request):
         serializer = TaskSerializer()
 
     tasks = Task.objects.all()
-    paginator = Paginator(tasks, 5)  # Set the number of tasks per page
+    search_query = request.GET.get('search')
 
+    if search_query:
+        tasks = tasks.filter(id=search_query)
+
+    paginator = Paginator(tasks, 5)  # Set the number of tasks per page
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
-    return render(request, 'create_task.html', {'serializer': serializer, 'tasks': tasks, 'page_obj': page_obj})
+    return render(request, 'create_task.html', {'serializer': serializer, 'tasks': tasks, 'page_obj': page_obj, 'search_results': tasks})
 
 
 def update_task(request, task_id):
